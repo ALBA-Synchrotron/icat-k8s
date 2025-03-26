0bf9e83d19dca9f18f5f34c49e4fd565878b03eb
Rodrigo Cabezas Quirós <rcabezas@cells.es>
2025-03-26 17:16:56 +0100

    added script install steps for other icat components

--- setup_scripts/icat_k8s_setup_generator.py
+++ setup_scripts/icat_k8s_setup_generator.py
@@ -8,9 +8,55 @@ args: dict = get_arguments()
 component: str = args["component"]
 
 prop_name: str = "run.properties"
-
+overwrite_files: list = [[prop_name, "WEB-INF/classes"]]
 asadmin_commands: list = []
+
 match component:
+    case "authn.anon":
+        if os.path.exists("logback.xml"): overwrite_files.append(["logback.xml", "WEB-INF/classes"])
+
+        deploy(deployment_order=80, files=overwrite_files)
+    case "authn.db":
+        prop_list: list = ["db.driver", "db.url", "db.username", "db.password"]
+        setup_props: dict = get_setup_parameters("setup.properties", prop_list)
+        db_commands: list = register_db(setup_props, "authn_db")
+        asadmin_commands.extend(db_commands)
+
+        if os.path.exists("logback.xml"): overwrite_files.append(["logback.xml", "WEB-INF/classes"])
+
+        deploy(deployment_order=80, files=overwrite_files)
+    case "authn.oidc":
+        prop_list: list = ["wellKnownUrl", "tokenIssuer", "icatUserClaim"]
+        run_props: dict = get_setup_parameters(prop_name, prop_list)
+
+        try:
+            icat_user_claim_exception: str = run_props["icatUserClaimException"]
+        except KeyError:
+            icat_user_claim_exception = ""
+
+        try:
+            mechanism: str = run_props["mechanism"]
+        except KeyError:
+            mechanism = ""
+
+        try:
+            icat_user_prepend_mechanism: str = run_props["icatUserPrependMechanism"]
+        except KeyError:
+            icat_user_prepend_mechanism = ""
+
+        if icat_user_prepend_mechanism == "true":
+            if mechanism == "":
+                sys.exit("icatUserPrependMechanism is 'true' but no mechanism is defined in run.properties")
+
+        if mechanism != "":
+            if icat_user_prepend_mechanism != "true":
+                if icat_user_claim_exception == "true":
+                    print(
+                        "Warning: the mechanism defined in run.properties is never used because icatUserPrependMechanism=false and icatUserClaimException=true")
+
+        if os.path.exists("logback.xml"): overwrite_files.append(["logback.xml", "WEB-INF/classes"])
+
+        deploy(deployment_order=80, files=overwrite_files)
     case "icat.server":
         prop_list: list = ["lifetimeMinutes", "rootUserNames", "authn.list", "notification.list", "log.list"]
         icat_properties: dict = get_properties(prop_name, prop_list)
@@ -36,7 +82,6 @@ match component:
         icat_jms_log: str = create_jms_resource("jakarta.jms.Topic", "jms/ICAT/log")
         asadmin_commands.append(icat_jms_log)
 
-        overwrite_files: list = [[prop_name, "WEB-INF/classes"]]
         if os.path.exists("logback.xml"): overwrite_files.append(["logback.xml", "WEB-INF/classes"])
 
         secure: bool = setup_props.get("secure", "false") == "true"
