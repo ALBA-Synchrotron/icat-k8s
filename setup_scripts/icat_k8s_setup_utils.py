53fab04552dc736e7bf24c37915edaa31e8d317c
Rodrigo Cabezas Quirós <rcabezas@cells.es>
2025-04-09 08:01:05 +0200

    asd

--- setup_scripts/icat_k8s_setup_utils.py
+++ setup_scripts/icat_k8s_setup_utils.py
@@ -78,7 +78,7 @@ def create_elastic_jvm_options(elastic_apm_jar_path: str = "/opt/payara/libs/ela
     if not packages_str:
         packages_str = "es.cells.icat.authn_alba,org.icatproject.authn_anon,org.icatproject.authn_db,org.icatproject.icat_oaipmh,org.icatproject.exposed,org.icatproject.ids"
 
-    ret.append(f"create-jvm-options '-javaagent\:{elastic_apm_jar_path}'")
+    ret.append(f"create-jvm-options '-javaagent:{elastic_apm_jar_path}'")
     ret.append(f"create-jvm-options '-Delastic.apm.service_name={os.getenv('APM_SERVICE_NAME')}'")
     ret.append(f"create-jvm-options '-Delastic.apm.application_packages={packages_str}'")
     ret.append(f"create-jvm-options '-Delastic.apm.server_url={os.getenv('ELASTIC_APM_SERVER_URL')}'")
